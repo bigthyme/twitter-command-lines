642a3bb15aa41f466dfbd7fce4b1f70ff41b8410
bigthyme <asinghavong@gmail.com>
2012-12-04 10:04:27 -0800

    Added search functionality

--- jstwitter.rb
+++ jstwitter.rb
@@ -11,12 +11,24 @@ class JSTwitter
 	end
 
 	def tweet(message)
-		@client.update(message)
 		#if message that will check if the tweet message is equal to 140.
 		if message.length >= 140
 			puts "People are yawning, please shorten your message"
 		else
 			puts "You've used " + message.length.to_s + " characters out of 140"
+		end
+		@client.update(message)
+	end
+
+	def dm(target, message)
+		puts "Trying to send #{target} this direct message:"
+		message = tweet("d #{target} #{message}")
+		puts message
+	end
+
+	def search(q, options={})
+		@client.search(q, :count => 50, :result_type => "recent").results.map do |status|
+				puts "#{status.from_user}: #{status.text}"
 		end
 	end
 
@@ -31,6 +43,8 @@ class JSTwitter
 			case command 
 				when 'q' then puts "Goodbye!"
 				when 't' then tweet(parts[1..-1].join(" "))
+				when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+				when 's' then search(parts[1..-1].join(" "))
 				else
 					puts "I'm sorry, I don't know how to #{command}"
 				end
